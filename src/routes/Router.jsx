946fda7063e16b2086db79753100b87be2afecaa
Trung Nam <trungnam.work@gmail.com>
2024-11-25 16:02:00 +0700

    Init project

---
+++ src/routes/Router.jsx
@@ -0,0 +1,44 @@
+import { createBrowserRouter } from "react-router-dom";
+import App from "../App";
+import Register from "../pages/Register";
+import CheckEmail from "../pages/CheckEmail";
+import CheckPassword from "../pages/CheckPassword";
+import Home from "../pages/Home";
+import Message from "../components/Message";
+
+
+
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+        children: [
+            { 
+                path: "", 
+                element: <Home/>,
+                children: [
+                    {
+                        path: ":userId",
+                        element:<Message/> 
+                    }
+                ]
+            },
+            { 
+                path: "/register", 
+                element: <Register/>
+            },
+            { 
+                path: "/email", 
+                element: <CheckEmail/>
+            },
+            { 
+                path: "/password", 
+                element: <CheckPassword/>
+            },
+        ]
+    }
+    
+]);
+
+export default router;
