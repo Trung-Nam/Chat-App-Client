ce9d357204b0e4e57733dd9452c186245f34c790
Trung Nam <trungnam.work@gmail.com>
2024-11-28 18:36:27 +0700

    fix view image and video

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -20,16 +20,17 @@ const Message = () => {
 
   const [message, setMessage] = useState({
     text: "",
-    imgUrl: "",
+    imageUrl: "",
     videoUrl: ""
-  });
+  })
+
   const [dataUser, setDataUser] = useState({
     name: "",
     email: "",
     profile_pic: "",
     online: false,
-    _id: "",
-  });
+    _id: ""
+  })
 
 
   useEffect(() => {
@@ -57,47 +58,47 @@ const Message = () => {
   }, [params?.userId, socketConnection, user])
 
   const handleUploadImage = async (e) => {
-    const file = e.target.files[0];
+    const file = e.target.files[0]
 
-    setLoading(true);
-    const uploadPhoto = await uploadFile(file);
-    setLoading(false);
-    setOpenImageVideoUpload(false);
+    setLoading(true)
+    const uploadPhoto = await uploadFile(file)
+    setLoading(false)
+    setOpenImageVideoUpload(false)
 
-    setMessage((prev) => {
+    setMessage(prev => {
       return {
         ...prev,
-        imgUrl: uploadPhoto?.url
-      }
-    })
-  }
-
-  const handleUploadVideo = async (e) => {
-    const file = e.target.files[0];
-
-    setLoading(true);
-    const uploadPhoto = await uploadFile(file);
-    setLoading(false);
-    setOpenImageVideoUpload(false);
-
-    setMessage((prev) => {
-      return {
-        ...prev,
-        videoUrl: uploadPhoto?.url
+        imageUrl: uploadPhoto.url
       }
     })
   }
 
   const handleClearUploadImage = () => {
-    setMessage((prev) => {
+    setMessage(prev => {
       return {
         ...prev,
-        imgUrl: ""
+        imageUrl: ""
+      }
+    })
+  }
+
+  const handleUploadVideo = async (e) => {
+    const file = e.target.files[0]
+
+    setLoading(true)
+    const uploadPhoto = await uploadFile(file)
+    setLoading(false)
+    setOpenImageVideoUpload(false)
+
+    setMessage(prev => {
+      return {
+        ...prev,
+        videoUrl: uploadPhoto.url
       }
     })
   }
   const handleClearUploadVideo = () => {
-    setMessage((prev) => {
+    setMessage(prev => {
       return {
         ...prev,
         videoUrl: ""
@@ -119,24 +120,23 @@ const Message = () => {
   }
 
   const handleSendMessage = (e) => {
-    e.preventDefault();
+    e.preventDefault()
 
-    if (message.text || message.imgUrl || message.videoUrl) {
+    if (message.text || message.imageUrl || message.videoUrl) {
       if (socketConnection) {
         socketConnection.emit('new message', {
           sender: user?._id,
           receiver: params.userId,
           text: message.text,
-          imgUrl: message.imgUrl,
+          imageUrl: message.imageUrl,
           videoUrl: message.videoUrl,
           msgByUserId: user?._id
         })
         setMessage({
           text: "",
-          imgUrl: "",
+          imageUrl: "",
           videoUrl: ""
         })
-
       }
     }
   }
@@ -153,7 +153,7 @@ const Message = () => {
             <Avatar
               width={45}
               height={45}
-              imgUrl={dataUser?.profile_pic}
+              imageUrl={dataUser?.profile_pic}
               name={dataUser?.name}
               userId={dataUser?._id}
             />
@@ -177,16 +177,49 @@ const Message = () => {
       {/* Show all message */}
       <div className="h-[calc(100vh-128px)] overflow-x-hidden overflow-y-scroll scrollbar relative bg-slate-200 bg-opacity-50">
 
+        {/* All message show here */}
+        <div className="flex flex-col gap-2 py-2 mx-2" ref={currentMessage}>
+          {
+            allMessages?.map((msg, index) => (
+              <div key={index} className={`bg-white p-1 py-1 rounded w-fit max-w-[280px] md:max-w-sm lg:max-w-md ${user?._id === msg?.msgByUserId ? "ml-auto bg-teal-100" : ""}`}>
+                <div className="w-full">
+                  {
+                    msg?.imageUrl && (
+                      <img
+                        src={msg?.imageUrl}
+                        className='w-full h-full object-scale-down'
+                      />
+                    )
+                  }
+                  {
+                    msg?.videoUrl && (
+                      <video
+                        src={msg?.videoUrl}
+                        className='w-full h-full object-scale-down'
+                        controls
+                      />
+                    )
+                  }
+                </div>
+                <p className="px-2">{msg.text}</p>
+                <p className="text-sm ml-auto w-fit">{moment(msg.createdAt).format('hh:mm')}</p>
+              </div>
+            )
+            )
+          }
+        </div>
+
+
         {/* Upload image display */}
         {
-          message?.imgUrl && (
-            <div className="w-full h-full bg-slate-700 bg-opacity-30 flex justify-center items-center rounded overflow-hidden">
+          message?.imageUrl && (
+            <div className="w-full h-full sticky bottom-0 bg-slate-700 bg-opacity-30 flex justify-center items-center rounded overflow-hidden">
               <div onClick={handleClearUploadImage} className="w-fit p-2 absolute top-0 right-0 cursor-pointer hover:text-primary text-white">
                 <IoClose size={25} />
               </div>
               <div className="bg-white p-3">
                 <img
-                  src={message?.imgUrl}
+                  src={message?.imageUrl}
                   alt="uploadImage"
                   className="aspect-square  w-full h-full max-w-sm m-2 object-scale-down"
                 />
@@ -197,7 +230,7 @@ const Message = () => {
         {/* Upload video display */}
         {
           message?.videoUrl && (
-            <div className="w-full h-full bg-slate-700 bg-opacity-30 flex justify-center items-center rounded overflow-hidden">
+            <div className="w-full h-full sticky bottom-0 bg-slate-700 bg-opacity-30 flex justify-center items-center rounded overflow-hidden">
               <div onClick={handleClearUploadVideo} className="w-fit p-2 absolute top-0 right-0 cursor-pointer hover:text-primary text-white">
                 <IoClose size={25} />
               </div>
@@ -216,25 +249,11 @@ const Message = () => {
 
         {
           loading && (
-            <div className="w-full h-full flex justify-center items-center">
+            <div className="w-full h-full sticky bottom-0 flex justify-center items-center">
               <Loading />
             </div>
           )
         }
-
-        {/* All message show here */}
-
-        <div className="flex flex-col gap-2 py-2 mx-2" ref={currentMessage}>
-          {
-            allMessages?.map((msg, index) => (
-              <div key={index} className={`bg-white p-1 py-1 rounded w-fit ${user?._id === msg?.msgByUserId ? "ml-auto bg-teal-100" : ""}`}>
-                <p className="px-2">{msg.text}</p>
-                <p className="text-sm ml-auto w-fit">{moment(msg.createdAt).format('hh:mm')}</p>
-              </div>
-            )
-            )
-          }
-        </div>
       </div>
 
       {/* Send message */}
