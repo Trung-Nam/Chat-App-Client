82b54ebe2590e39718b119666e6163dcc45bce2b
Trung Nam <trungnam.work@gmail.com>
2024-11-28 21:38:30 +0700

    List of conversations

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { IoChatbubbleEllipses } from "react-icons/io5";
 import { FaUserPlus } from "react-icons/fa";
 import { FiArrowUpLeft } from "react-icons/fi";
@@ -9,11 +9,44 @@ import { useSelector } from 'react-redux';
 import EditUserDetails from './EditUserDetails';
 import Divider from './Divider';
 import SearchUser from './SearchUser';
+import { FaImage, FaVideo } from 'react-icons/fa6';
 const Sidebar = () => {
   const user = useSelector(state => state?.user);
   const [editUserOpen, setEditUserOpen] = useState(false);
   const [allUsers, setAllUsers] = useState([]);
   const [openSearchUser, setOpenSearchUser] = useState(false);
+  const socketConnection = useSelector(state => state?.user?.socketConnection);
+
+  useEffect(() => {
+    if (socketConnection) {
+      socketConnection.emit('sidebar', user._id);
+
+      socketConnection.on('conversation', (data) => {
+        const conversationUserData = data.map((conversationUser) => {
+          if (conversationUser?.sender?._id === conversationUser?.receiver?._id) {
+            return {
+              ...conversationUser,
+              userDetails: conversationUser?.sender
+            }
+          } else if (conversationUser?.receiver?._id !== user?._id) {
+            return {
+              ...conversationUser,
+              userDetails: conversationUser?.receiver
+            }
+          } else {
+            return {
+              ...conversationUser,
+              userDetails: conversationUser?.sender
+            }
+          }
+
+        })
+        setAllUsers(conversationUserData);
+      })
+    }
+
+  }, [socketConnection, user])
+
 
   return (
     <div className="w-full h-full grid grid-cols-[48px,1fr] bg-white">
@@ -69,6 +102,46 @@ const Sidebar = () => {
                 <p className="text-lg text-center text-slate-400">Explore users to start a conversation with.</p>
               </div>
             )
+          }
+
+          {
+            allUsers?.map((conversation) => (
+              <NavLink to={`/${conversation?.userDetails?._id}`} key={conversation?._id} className='flex items-center gap-2 p-2 border border-transparent rounded hover:bg-slate-200 cursor-pointer'>
+                <div>
+                  <Avatar
+                    imgUrl={conversation?.userDetails?.profile_pic}
+                    name={conversation?.userDetails?.name}
+                    height={40}
+                    width={40}
+                  />
+                </div>
+                <div>
+                  <h3 className="text-ellipsis line-clamp-1 font-semibold text-base">{conversation?.userDetails?.name}</h3>
+                  <div className="text-xs text-slate-500 flex items-center gap-1">
+                    <div className="flex items-center gap-1">
+                      {
+                        conversation?.lastMsg?.imageUrl && (
+                          <div className="flex items-center gap-1">
+                            <span><FaImage /></span>
+                            {!conversation?.lastMsg?.text && <span>Image</span>}
+                          </div>
+                        )
+                      }
+                      {
+                        conversation?.lastMsg?.videoUrl && (
+                          <div className="flex items-center gap-1">
+                            <span><FaVideo /></span>
+                            {!conversation?.lastMsg?.text && <span>Video</span>}
+                          </div>
+                        )
+                      }
+                    </div>
+                    <p>{conversation?.lastMsg?.text}</p>
+                  </div>
+                </div>
+                <p className="text-xs w-6 h-6 flex justify-center items-center ml-auto p-1 bg-primary text-white rounded-full">{conversation?.unseenMsg}</p>
+              </NavLink>
+            ))
           }
         </div>
       </div>
